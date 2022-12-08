0b9f54ea0c750065b75b9b206dc76f09e723e076
hrtchoke <hrtchoke@hrt.choke>
2022-12-08 14:13:32 -0800

    5

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = ['0.0.0.0:3000']
+CORS_ALLOWED_ORIGINS = ['https://0.0.0.0:3000']
 
 # Application definition
 
