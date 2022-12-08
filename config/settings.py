c5d5eb6e9ca4b13dec8f2bb89ba943835c0a5285
hrtchoke <hrtchoke@hrt.choke>
2022-12-08 14:03:02 -0800

    3

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000']
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://0.0.0.0:3000/']
 
 # Application definition
 
